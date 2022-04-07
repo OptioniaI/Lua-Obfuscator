539be7c09d2fcd5c7219cfc309ae135b001acf2e
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2022-04-07 14:32:29 -0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,8 @@
-print("Lua Obfuscator by Pyan")
+local me = game.Players.LocalPlayer.Character.HumanoidRootPart
+local other = game.Workspace.USERNAME.HumanoidRootPart
+
+_G.GUARD = true
+while _G.GUARD == true do
+  task.wait()
+  me.CFrame = other.CFrame * CFrame.new(0, 0, -17.5)
+end
